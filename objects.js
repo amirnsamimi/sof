e5df1998cf3eb99d871bcc239b3591d9f516ec6b
amirnsamimi <amirnsamimi@gmail.com>
2024-07-11 14:53:03 +0330

    bubble sort added

--- objects.js
+++ objects.js
@@ -1,115 +1,139 @@
 const students = [
-    {
-      name: "Davina J Eskew",
-      gender: "famale",
-      age: 61,
-      courses: ["math", "bio"],
-    },
-    {
-      name: "Jordan M Johnson",
-      gender: "male",
-      age: 67,
-      courses: ["math", "chemistry"],
-    },
-    {
-      name: "James G Carmona",
-      gender: "male",
-      age: 25,
-      courses: ["bio", "computer"],
-    },
-    {
-      name: "Orville S Schuyler",
-      gender: "male",
-      age: 59,
-      courses: ["computer", "bio"],
-    },
-    {
-      name: "Monica H Hill",
-      gender: "female",
-      age: 37,
-      courses: ["computer", "math", "chemistry"],
-    },
-    {
-      name: "Zachary J Hawkins",
-      gender: "male",
-      age: 34,
-      courses: ["bio"],
-    },
-    {
-      name: "Elizabeth J Mercer",
-      gender: "female",
-      age: 12,
-      courses: [],
-    },
-  ];
-  
-  
-  
-  // دانش‌آموزانی که سنشون کمتر از ۵۰ ساله و شیمی خوندن
-  const studentsBelow50andChem = students.filter(
-    (student) => student.age < 50 && student.courses.includes("chemistry")
-  );
-  console.group("Students that are more than 50 and studied Chemistry");
-  console.log(studentsBelow50andChem);
-  console.groupEnd();
-  
-  // دانش آموزانی که مرد هستن و ریاضی خوندن
-  const studentsMaleandMath = students.filter(
-    (student) => student.gender === "male" && student.courses.includes("math")
-  );
-  console.group(" Students which are male and studied Math");
-  console.log(studentsMaleandMath);
-  console.groupEnd();
-  
-  // دانش‌آموزانی که سنشون بیشتر از ۴۰ ساله، کامپیوتر خوندن ولی ریاضی نخوندن
-  const studentsAbove40andComputerButNotMath = students.filter(
-    (student) =>
-      student.age > 40 &&
-      student.courses.includes("computer") &&
-      !student.courses.includes("math")
-  );
-  console.group("Students that are more than 40 and Computer but not Math");
-  console.log(studentsAbove40andComputerButNotMath);
-  console.groupEnd();
-  
-  // دانش آموزانی که هیچی نخوندن
-  const studentsStudiedNothing = students.filter(
-    (student) => student.courses.length < 1
-  );
-  console.group("Students that studied nothing");
-  console.log(studentsStudiedNothing);
-  console.groupEnd();
-  
-  // آیا توی لیست دانش‌آموزان، دانش‌آموزی داریم که اسمش الیزابت باشه؟
-  let ElizabethInStudents = false;
-  students.map((student) => {
-    if (student.name.includes("Elizabeth") == true) {
-      ElizabethInStudents = true;
+  {
+    name: "Davina J Eskew",
+    gender: "famale",
+    age: 61,
+    courses: ["math", "bio"],
+  },
+  {
+    name: "Jordan M Johnson",
+    gender: "male",
+    age: 67,
+    courses: ["math", "chemistry"],
+  },
+  {
+    name: "James G Carmona",
+    gender: "male",
+    age: 25,
+    courses: ["bio", "computer"],
+  },
+  {
+    name: "Orville S Schuyler",
+    gender: "male",
+    age: 59,
+    courses: ["computer", "bio"],
+  },
+  {
+    name: "Monica H Hill",
+    gender: "female",
+    age: 37,
+    courses: ["computer", "math", "chemistry"],
+  },
+  {
+    name: "Zachary J Hawkins",
+    gender: "male",
+    age: 34,
+    courses: ["bio"],
+  },
+  {
+    name: "Elizabeth J Mercer",
+    gender: "female",
+    age: 12,
+    courses: [],
+  },
+];
+
+// دانش‌آموزانی که سنشون کمتر از ۵۰ ساله و شیمی خوندن
+const studentsBelow50andChem = students.filter(
+  (student) => student.age < 50 && student.courses.includes("chemistry")
+);
+console.group("Students that are more than 50 and studied Chemistry");
+console.log(studentsBelow50andChem);
+console.groupEnd();
+
+// دانش آموزانی که مرد هستن و ریاضی خوندن
+const studentsMaleandMath = students.filter(
+  (student) => student.gender === "male" && student.courses.includes("math")
+);
+console.group(" Students which are male and studied Math");
+console.log(studentsMaleandMath);
+console.groupEnd();
+
+// دانش‌آموزانی که سنشون بیشتر از ۴۰ ساله، کامپیوتر خوندن ولی ریاضی نخوندن
+const studentsAbove40andComputerButNotMath = students.filter(
+  (student) =>
+    student.age > 40 &&
+    student.courses.includes("computer") &&
+    !student.courses.includes("math")
+);
+console.group("Students that are more than 40 and Computer but not Math");
+console.log(studentsAbove40andComputerButNotMath);
+console.groupEnd();
+
+// دانش آموزانی که هیچی نخوندن
+const studentsStudiedNothing = students.filter(
+  (student) => student.courses.length < 1
+);
+console.group("Students that studied nothing");
+console.log(studentsStudiedNothing);
+console.groupEnd();
+
+// آیا توی لیست دانش‌آموزان، دانش‌آموزی داریم که اسمش الیزابت باشه؟
+let ElizabethInStudents = false;
+students.map((student) => {
+  if (student.name.includes("Elizabeth") == true) {
+    ElizabethInStudents = true;
+  }
+});
+console.group("is Elizabeth in students");
+console.log(ElizabethInStudents);
+console.groupEnd();
+
+// آیا توی لیست دانش‌آموزان، دانش‌آموزی رو داریم که سنش بیشتر از ۶۰ باشه و مرد باشه؟
+doWeHaveMaleAbove60 = false;
+students.map((student) => {
+  if (student.gender == "male" && student.age > 60) {
+    doWeHaveMaleAbove60 = true;
+  }
+});
+console.group("is Elizabeth in students");
+console.log(doWeHaveMaleAbove60);
+console.groupEnd();
+
+// درس‌هایی رو که تمام دانش‌آموزان خانوم دارن میخونن رو لیست کنین
+const femaleStudying = [];
+const femaleStudendsStudying = students
+  .filter((student) => student.gender === "female")
+  .map((course, index) => femaleStudying.push(...course.courses));
+console.group("list of courses taken by females");
+console.log(femaleStudying);
+console.groupEnd();
+
+
+const sortedList = () => {
+const cStudents = [...students];
+let timePassed = 0
+let temp = {};
+const timeConsumption = setInterval(()=>{
+  timer++
+},1000)
+cStudents.forEach(()=>{
+  cStudents.forEach((stu1, i) => {
+    if (i + 1 < cStudents.length) {
+      if (cStudents[i].age > cStudents[i + 1].age) {
+        temp = cStudents[i];
+        cStudents[i] = cStudents[i+1];
+        cStudents[i + 1] = temp;
+        temp = {};
+      
+      }
     }
   });
-  console.group("is Elizabeth in students");
-  console.log(ElizabethInStudents);
-  console.groupEnd();
-  
-  // آیا توی لیست دانش‌آموزان، دانش‌آموزی رو داریم که سنش بیشتر از ۶۰ باشه و مرد باشه؟
-  doWeHaveMaleAbove60 = false;
-  students.map((student) => {
-    if (student.gender == "male" && student.age > 60) {
-      doWeHaveMaleAbove60 = true;
-    }
-  });
-  console.group("is Elizabeth in students");
-  console.log(doWeHaveMaleAbove60);
-  console.groupEnd();
-  
-  // درس‌هایی رو که تمام دانش‌آموزان خانوم دارن میخونن رو لیست کنین
-  const femaleStudying = [];
-  const femaleStudendsStudying = students
-    .filter((student) => student.gender === "female")
-    .map((course, index) => femaleStudying.push(...course.courses));
-  console.group("list of courses taken by females");
-  console.log(femaleStudying);
-  console.groupEnd();
-  
-  
-  
+})
+  clearInterval(timeConsumption)
+  console.log(timePassed)
+  return cStudents
+};
+console.group("Sorted List");
+console.log(sortedList());
+console.groupEnd();
